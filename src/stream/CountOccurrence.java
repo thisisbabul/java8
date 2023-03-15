fe48de03321708f892bad485d583ff1d941b7b33
babul <babul.cu.cse35@gmail.com>
2023-03-15 23:44:37 +0600

    java program count the occurrence of each character in a string

--- src/stream/CountOccurrence.java
+++ src/stream/CountOccurrence.java
@@ -13,3 +13,5 @@ public class CountOccurrence {
         System.out.println(result);
     }
 }
+/*output
+{a=2, b=1, c=1, d=1, e=2, g=1, h=1, i=1, l=2, m=1, n=2, o=2, r=1, s=1, t=1, u=1, v=1, y=2}*/
